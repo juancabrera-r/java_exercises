e5cbbd4e68f5fc681166c97b50aac498327f191d
Juan Manuel <juanmanuelcabrera.r@gmail.com>
2024-08-04 12:00:01 +0100

    Modify ProgramaManejoDeNombres

--- src/ProgramaManejoDeNombres.java
+++ src/ProgramaManejoDeNombres.java
@@ -11,15 +11,12 @@ public class ProgramaManejoDeNombres {
 
     public static String modifyName(String name){
 
-        char first_letter = name.toUpperCase().charAt(1);
-        char[] nameA = name.toCharArray();
+        int len = name.length();
 
-        int len = nameA.length;
-        
-        char last_letter1 = name.charAt(len-1);
-        char last_letter2 = name.charAt(len-2);
-        
-        return (first_letter + "." + last_letter1 + last_letter2);
+        char first_letter = name.toUpperCase().charAt(1);
+        String lastTwoLetters = name.substring(len - 2);
+                
+        return (first_letter + "." + lastTwoLetters);
     }    
 
     public static void main(String[] args) {
@@ -31,7 +28,7 @@ public class ProgramaManejoDeNombres {
         }
 
         for (int i=0; i<name.length; i++){            
-            System.out.println(modifyName(name[i]) + "_");
+            System.out.print(modifyName(name[i]) + (i< name.length -1? "_": ""));
         }
         
     }
